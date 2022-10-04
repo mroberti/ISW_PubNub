2b282305bd7dfb86e840087214eb315617e1c288
Mario Roberti <mario.roberti@gmail.com>
2022-10-04 08:58:50 -0700

    Updated dependencies, PubNub and Phaser versions
    Updated dependencies, PubNub and Phaser versions

--- game.js
+++ game.js
@@ -1,117 +1,161 @@
-const gameState = {
-	score: 0
-}
-
-function preload () {
-	this.load.image('platform', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/platform.png');
-	this.load.image('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/codey.png');
-	this.load.multiatlas('ships', 'assets/ships.json', 'assets');
-}
-
-function create () {
-	pubnub.PubNub({
-		subscribeKey: string,
-		publishKey: string,
-		cipherKey: string,
-		authKey: string,
-		logVerbosity: boolean,
-		userId: string
-		ssl: boolean,
-		origin: string,
-		presenceTimeout: number,
-		heartbeatInterval: number,
-		restore: boolean,
-		keepAlive: boolean,
-		keepAliveSettings: any,
-		useInstanceId: boolean,
-		suppressLeaveEvents: boolean,
-		requestMessageCountThreshold: number,
-		autoNetworkDetection: boolean,
-		listenToBrowserNetworkEvents: boolean
-		// Deprecated, use userId instead
-		userId: string,
-	  });
-	  
-	const platforms = this.physics.add.staticGroup();
- 
-	platforms.create(320, 350, 'platform').setScale(2, 0.5).refreshBody();
-
-	gameState.scoreText = this.add.text(320, 340, 'Score: 0', { fontSize: '15px', fill: '#000' })
-
-	this.player = this.physics.add.sprite(320, 300, 'ships', 'e7 titan.png').setScale(.15);
-	
-	this.player.setCollideWorldBounds(true);
-
-	this.physics.add.collider(this.player, platforms)
-
-	const bugs = this.physics.add.group();
-
-	const bugList = ['e7 titan.png', 'e2 titan.png', 'e1 titan.png']
-
-	const bugGen = () => {
-		const xCoord = Math.random() * 640
-		let randomBug = bugList[Math.floor(Math.random() * 3)]
-		bugs.create(xCoord, 10, randomBug)
-	}
-
-	const bugGenLoop = this.time.addEvent({
-		delay: 100,
-		callback: bugGen,
-		loop: true,
-	});
-
-
-	this.physics.add.collider(bugs, platforms, function (bug){
-		bug.destroy();
-		gameState.score += 10;
-		gameState.scoreText.setText(`Score: ${gameState.score}`)		
-	})
-
-	this.physics.add.collider(this.player, bugs, () => {
-			bugGenLoop.destroy();
-			this.physics.pause();
-
-			this.add.text(280, 150, 'Game Over \n Click to Restart', { fontSize: '15px', fill: '#000' })
-			gameState.score = 0
-
-			this.input.on('pointerdown', () => {
-				this.scene.restart();
-			})
-	})
-
-}
-
-function update () {
-	const cursors = this.input.keyboard.createCursorKeys();
-
-	if(cursors.left.isDown){
-		this.player.setVelocityX(-200)
-	} else if (cursors.right.isDown) {
-		this.player.setVelocityX(200)
-	} else {
-		this.player.setVelocityX(0);
-	}
-
-}
+const rand = Phaser.Math.Between;
 
 const config = {
   type: Phaser.AUTO,
-  width: 1280,
-	height: 800,
-	backgroundColor: "b9eaff",
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: {y: 200},
-			enableBody: true,
-			debug: false,
-		}
-	},
+  width: 800,
+  height: 600,
+  parent: "game-container",
+  pixelArt: true,
   scene: {
-		preload,
-		create,
-		update
-	}
+	preload: preload,
+	create: create,
+	update: update
+  }
+};
+
+const game = new Phaser.Game(config);
+let controls;
+var ships = []
+var shipGroup
+var groupconfig = {
+    classType: Phaser.GameObjects.Sprite,
+    defaultKey: null,
+    defaultFrame: null,
+    active: true,
+    maxSize: -1,
+    runChildUpdate: true,
+    createCallback: null,
+    removeCallback: null,
+    createMultipleCallback: null
 }
 
-const game = new Phaser.Game(config)
+function preload() {
+	this.load.multiatlas('allships', '/assets/ships/allships.json', 'assets');
+	this.load.image("background", "/assets/backgrounds/background2.jpg");
+	// in preload()
+	this.load.json('sheetdata', '/assets/ships/allships.json');
+}
+
+function MakeDraggable(theSprite,passedThis,passedCamera){
+	// Let's put them randomly somewhere for now...
+
+	theSprite.x = rand(1,passedCamera.width);
+	theSprite.y = rand(1,passedCamera.height);
+	theSprite.angle = rand(0,359);
+	theSprite.setScale(.5);
+	theSprite.on('pointerover', function () {
+		this.setTint(0x00ff00);
+	});
+
+	theSprite.on('pointerout', function () {
+		this.clearTint();
+	});
+
+	passedThis.input.setDraggable(theSprite);
+	passedThis.input.on('dragstart', function (pointer, gameObject) {
+		gameObject.setTint(0xff0000);
+	});
+
+	passedThis.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+		gameObject.x = dragX;
+		gameObject.y = dragY;
+	});
+
+	passedThis.input.on('dragend', function (pointer, gameObject) {
+		gameObject.clearTint();
+	});
+}
+
+function BackgroundScroll(theSprite,passedThis){
+	theSprite.setScale(.5);
+	theSprite.on('pointerover', function () {
+		this.setTint(0x00ff00);
+	});
+
+	theSprite.on('pointerout', function () {
+		this.clearTint();
+	});
+
+	passedThis.input.setDraggable(theSprite);
+	passedThis.input.on('dragstart', function (pointer, gameObject) {
+		gameObject.setTint(0xff0000);
+	});
+
+	passedThis.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+		gameObject.tilePositionX = dragX;
+		gameObject.tilePositionY  = dragY;
+	});
+
+	passedThis.input.on('dragend', function (pointer, gameObject) {
+		gameObject.clearTint();
+	});
+}
+
+function create() {
+	// in create()
+	let data = this.cache.json.get('sheetdata');
+
+	// Phaser supports multiple cameras, but you can access the default camera like this:
+	const camera = this.cameras.main;
+	shipGroup = this.make.group(groupconfig);
+
+
+	// Set up the arrows to control the camera
+	const cursors = this.input.keyboard.createCursorKeys();
+	controls = new Phaser.Cameras.Controls.FixedKeyControl({
+		camera: camera,
+		left: cursors.left,
+		right: cursors.right,
+		up: cursors.up,
+		down: cursors.down,
+		speed: 0.5
+	});
+
+
+
+	var background = this.add.tileSprite(0, 0, camera.width, camera.height, 'background').setInteractive();
+	var ships = ["e2 titan.png","e3 destroyer.png"];
+	console.log("The stuff "+data.textures[0].frames[0].filename);
+	var string = "foo",
+    substring = "oo";
+	console.log(string.includes(substring));
+	for (let index = 0; index < data.textures[0].frames.length; index++) {
+		var fileName = data.textures[0].frames[index].filename
+		if(fileName.includes("e2")){
+			var tempShip = this.add.sprite(0, 0, 'allships', fileName).setInteractive();
+			MakeDraggable(tempShip,this,camera);
+			shipGroup.add(tempShip);
+			// group.add(gameObject, true);  // add this game object to display and update list of scene
+		}
+	}	
+	
+	// Help text that has a "fixed" position on the screen
+	this.add
+	.text(16, 16, "Arrow keys to scroll", {
+		font: "18px monospace",
+		fill: "#ffffff",
+		padding: { x: 20, y: 10 },
+		backgroundColor: "#00000000"
+	})
+	.setScrollFactor(0);
+}
+
+function update(time, delta) {
+	// Apply the controls to the camera each update tick of the game
+	controls.update(delta);
+	var gameObjects = shipGroup.getChildren();  // array of game objects
+	for (let index = 0; index < gameObjects.length; index++) {
+		tempShip = gameObjects[index];
+
+		var speed_angle = tempShip.angle;
+		var speed_length = 0.25;
+		var tempSpeed = Phaser.Math
+
+		var speed_x = speed_length * Math.cos(Phaser.Math.DegToRad(speed_angle));
+		var speed_y = speed_length * Math.sin(Phaser.Math.DegToRad(speed_angle));
+		tempShip.x = tempShip.x + speed_x;
+		tempShip.y = tempShip.y + speed_y;
+
+		
+	}
+}
